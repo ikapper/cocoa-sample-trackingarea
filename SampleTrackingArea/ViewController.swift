f7d209bb29ac7d3c253a54291f1cfb7794dc63d7
ikapper <ikapdev@gmail.com>
2017-10-09 23:42:51 +0900

    Initial Commit

---
+++ SampleTrackingArea/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SampleTrackingArea
+//
+//  Created by LoopMac on 2017/10/09.
+//  Copyright © 2017年 ikapper. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
